00ff2a8579c14748f301da6a16f104a9a3cf7117
VijayaSalagundi350 <151591833+VijayaSalagundi350@users.noreply.github.com>
2023-11-22 08:54:00 -0800

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 
-streamlit.title('My Mom's new Healthy Diner')
+streamlit.title('My Moms new Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega3 and Blueberry Oatmeal')
 streamlit.text('Kale, spinach & Rocket smoothie')
 streamlit.text('Hard-boiled free-ranged eggs')
 streamlit.text('Avacado Toast')
 streamlit.header('Build your own Fruit Smoothie')
-
